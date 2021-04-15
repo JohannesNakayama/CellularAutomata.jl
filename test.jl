b1cbacecc9889e324054ee515a67bb8712624114
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2021-04-15 19:39:34 +0200

    some experiments with graph representations

--- test.jl
+++ test.jl
@@ -79,14 +79,14 @@ gcf()
 automaton = []
 
 # initial configuration
-iter = vcat(zeros(Int, 100), [1], zeros(Int, 100))
+iter = vcat(zeros(Int, 200), [1], zeros(Int, 200))
 push!(automaton, copy(iter))
 
 # construct graph
 g = LightGraphs.grid([length(iter), 1], periodic=true)
 
 # run and display automaton
-for i in 1:100
+for i in 1:200
     tmp = next_iteration(iter, g, 30)
     push!(automaton, copy(tmp))
     iter = copy(tmp)
@@ -101,8 +101,23 @@ gcf()
 
 
 
+### EXAMPLE 4: RULE 30 ###
 
+# push iterations into this array
+automaton = []
 
+# initial configuration
+iter = vcat(zeros(Int, 100), ones(Int, 1), zeros(Int, 100))
+push!(automaton, copy(iter))
 
+# construct graph
+g = LightGraphs.grid([length(iter), 1], periodic=true)
 
-
+# run and display automaton
+for i in 1:100
+    tmp = next_iteration(iter, g, 30)
+    push!(automaton, copy(tmp))
+    iter = copy(tmp)
+end
+imshow(automaton, cmap = "gray")
+gcf()
