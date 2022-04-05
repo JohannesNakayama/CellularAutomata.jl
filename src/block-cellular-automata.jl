a0c9043cc3007e0b17626263e7543deb17e9586f
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-04-05 11:20:16 +0200

    Isolated BCA step function

--- src/block-cellular-automata.jl
+++ src/block-cellular-automata.jl
@@ -1,30 +1,52 @@
 mutable struct BlockCellularAutomaton
     states::Array{Any}
-    function Automaton(init_state::AbstractMatrix, init_start::Int)
+    function BlockCellularAutomaton(init_state::AbstractMatrix, init_start::Int)
         new([(init_start, init_state)])
     end
 end
 
 function run!(automaton::BlockCellularAutomaton, steps::Int, rule::Function)
     for step in 1:steps
-        curr_state = deepcopy(automaton.states[end][2])
-        start = automaton.states[end][1] == 1 ? 2 : 1
-        next_state = zeros(Bool, size(curr_state))
-        Threads.@threads for i in start:2:size(curr_state)[1]
-            for j in start:2:size(curr_state)[2]
-                pos1 = [i, j]
-                pos2 = [i, j+1 <= size(curr_state)[2] ? j+1 : 1]
-                pos3 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j]
-                pos4 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j+1 <= size(curr_state)[2] ? j+1 : 1]
-                block = [
-                    curr_state[pos1...] curr_state[pos2...];
-                    curr_state[pos3...] curr_state[pos4...]
-                ]
-                next_state = rule(curr_state, next_state, block, pos1, pos2, pos3, pos4)
-            end
-        end
-        push!(automaton.states, (copy(start), copy(next_state)))
+        # curr_state = deepcopy(automaton.states[end][2])
+        # start = automaton.states[end][1] == 1 ? 2 : 1
+        # next_state = zeros(Bool, size(curr_state))
+        # Threads.@threads for i in start:2:size(curr_state)[1]
+        #     for j in start:2:size(curr_state)[2]
+        #         pos1 = [i, j]
+        #         pos2 = [i, j+1 <= size(curr_state)[2] ? j+1 : 1]
+        #         pos3 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j]
+        #         pos4 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j+1 <= size(curr_state)[2] ? j+1 : 1]
+        #         block = [
+        #             curr_state[pos1...] curr_state[pos2...];
+        #             curr_state[pos3...] curr_state[pos4...]
+        #         ]
+        #         next_state = rule(curr_state, next_state, block, pos1, pos2, pos3, pos4)
+        #     end
+        # end
+        # push!(automaton.states, (copy(start), copy(next_state)))
+        step!(automaton, rule)
     end
+    return automaton
+end
+
+function step!(automaton::BlockCellularAutomaton, rule::Function)
+    curr_state = deepcopy(automaton.states[end][2])
+    start = automaton.states[end][1] == 1 ? 2 : 1
+    next_state = zeros(Bool, size(curr_state))
+    Threads.@threads for i in start:2:size(curr_state)[1]
+        for j in start:2:size(curr_state)[2]
+            pos1 = [i, j]
+            pos2 = [i, j+1 <= size(curr_state)[2] ? j+1 : 1]
+            pos3 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j]
+            pos4 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j+1 <= size(curr_state)[2] ? j+1 : 1]
+            block = [
+                curr_state[pos1...] curr_state[pos2...];
+                curr_state[pos3...] curr_state[pos4...]
+            ]
+            next_state = rule(curr_state, next_state, block, pos1, pos2, pos3, pos4)
+        end
+    end
+    push!(automaton.states, (copy(start), copy(next_state)))
     return automaton
 end
 
